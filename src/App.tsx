801728afc8889f30417b8f6e2e2928df6aa82042
paul-frdev <pavelzadyrko@gmail.com>
2022-06-03 00:02:40 +0300

    add skeleton routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { HomePage } from './pages/HomePage';
-
+import { BrowserRouter as Router } from "react-router-dom";
 
 import './styles/scss/app.scss';
 
--- src/containers/PizzaList.tsx
+++ src/containers/PizzaList.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { PizzaBlock } from '../components';
+import { Skeleton } from '../components/pizzaBlock/Skeleton';
 import { useAppDispatch, UseAppSelector } from '../hooks/hook';
 import { fetchGoods } from '../store/goodsSlice';
 
 
 export const PizzaList = () => {
-  const { goods } = UseAppSelector(state => state.goods);
+  const { goods, loading } = UseAppSelector(state => state.goods);
   const dispatch = useAppDispatch();
 
   React.useEffect(() => {
-    dispatch(fetchGoods())
-  }, [])
+    dispatch(fetchGoods());
+  }, []);
 
 
   return (
     <>
-      {Object.values(goods).map(product => (
-        <PizzaBlock key={product.id} productItem={product} />
-      ))}
+      {
+        loading ? Array(6).fill(<></>).map((_, index) => <Skeleton key={index} />) :
+          Object.values(goods).map(product => (
+            <PizzaBlock key={product.id} productItem={product} />
+          ))
+
+      }
     </>
   )
 };
